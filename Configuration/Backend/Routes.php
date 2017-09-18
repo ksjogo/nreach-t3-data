0fc56fb0fa3a2b3cbadcce973a1d514628355df0
ksjogo <jogo@kronberger-spiele.de>
2017-09-18 14:38:08 +0200

    Fix Route

--- Configuration/Backend/Routes.php
+++ Configuration/Backend/Routes.php
@@ -2,10 +2,9 @@
 use Nreach\T3Data\Controller\SingleController;
 
 return [
-    // Login screen of the TYPO3 Backend
-    'login' => [
-                'path' => '/entity',
-                'access' => 'public',
-                'target' => SingleController::class . '::indexAction'
+    'entity' => [
+        'path' => '/entity',
+        'access' => 'public',
+        'target' => SingleController::class . '::indexAction'
     ],
 ];
